7d38fb38538baea50f1472e5bec205f0988c97f2
Aiman Al Eryany <102668388+aiman99aleryany@users.noreply.github.com>
2022-09-22 01:00:01 +0300

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,12 +1,14 @@
-// starting from the exercise n. 31, we want to print the "hello" "name" strings after one second. in order to achieve this, we can use the settimeout() javascript function. this function takes in two parameters: the first one is a function, the second one is the delay time.
+// starting from the exercise n. 31, we want to print the "hello" "name" strings after one second. 
+//in order to achieve this, we can use the settimeout() javascript function. this function takes in two parameters: the first one is a function, 
+//the second one is the delay time.
 
 function sayHelloName(func, delayTime) {
-  setTimeout(func, delayTime);
+  let name = "Aiman"
+  setTimeout(() => func(name), delayTime); // we use arrow function here which contains the call for the "printName" function, because I can't pass the callback function with an argument to the setTimeout function
   console.log("Hello");
 }
 
 function printName(name) {
-  // name will be undefined here :)
   console.log(name);
 }
 
